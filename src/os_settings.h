afb2f2129bffb22f11c7217e1adf27b2ef05388c
bseppke <benjamin.seppke@gmail.com>
2016-03-11 10:32:17 +0100

    removed linker flag for export/import, since this lib should not be included in other c/c++ projects

--- src/os_settings.h
+++ src/os_settings.h
@@ -1,13 +1,5 @@
-#ifdef VIGRA_C_BUILD
-    #ifdef _WIN32
-        #define LIBEXPORT  extern "C" __declspec(dllexport)
-    #else
-        #define LIBEXPORT  extern "C"
-    #endif
+#ifdef _WIN32
+    #define LIBEXPORT  extern "C" __declspec(dllexport)
 #else
-    #ifdef _WIN32
-        #define LIBEXPORT __declspec(dllimport)
-    #else
-        #define LIBEXPORT
-    #endif
+    #define LIBEXPORT  extern "C"
 #endif
